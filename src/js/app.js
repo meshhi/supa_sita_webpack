d1e824bfe95ae0369fa953d2a9ea9002e0d6b187
meshhi <yumythug@yandex.ru>
2023-10-17 02:51:31 +0300

    init

--- src/js/app.js
+++ src/js/app.js
@@ -2,20 +2,20 @@
 
 // comment this to pass build
 const isHidden = (el) => {
-  return (el.offsetParent === null)
-}
+  return el.offsetParent === null;
+};
 
-document.addEventListener('scroll', (e) => {
-  const blocks = document.querySelectorAll('.custom-block');
-  blocks.forEach(block => {
-      if(isHidden(block)) return;
-      if(block.classList.contains('appear')) return;
-      if(block.classList.contains('about')) return;
-      if(block.classList.contains('start')) return;
-      if(block.classList.contains('contacts')) {
-          block.classList.add('appear-bottom');
-          return;
-      };
-      block.classList.add('appear');
-  })
-})
+document.addEventListener("scroll", () => {
+  const blocks = document.querySelectorAll(".custom-block");
+  blocks.forEach((block) => {
+    if (isHidden(block)) return;
+    if (block.classList.contains("appear")) return;
+    if (block.classList.contains("about")) return;
+    if (block.classList.contains("start")) return;
+    if (block.classList.contains("contacts")) {
+      block.classList.add("appear-bottom");
+      return;
+    }
+    block.classList.add("appear");
+  });
+});
